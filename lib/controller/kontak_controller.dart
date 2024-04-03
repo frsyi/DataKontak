14ebd3f6c51967fc131ba57bf4c20e4322e0e2d5
Fahma Rosyidah <fahmarosyi@gmail.com>
2024-04-03 21:22:02 +0700

    menambahkan method addPerson di class KontakController

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -1,5 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:data_kontak/model/kontak.dart';
 import 'package:data_kontak/service/kontak_service.dart';
 
 class KontakController {
   final kontakService = KontakService();
+
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
+    Map<String, String> data = {
+      'nama': person.nama,
+      'email': person.email,
+      'alamat': person.alamat,
+      'no_telepon': person.noTelepon,
+    };
+
+    try {
+      var response = await kontakService.addPerson(data, file);
+
+      if (response.statusCode == 201) {
+        return {'succes': true, 'message': 'Data berhasil disimpan'};
+      } else {
+        //Penanganan ketika Content-Type bukan application/json
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'succes': false,
+            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
+          };
+        }
+
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'succes': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi kesalahan saat menyimpan data',
+        };
+      }
+    } catch (e) {
+      //Menangkap kesalahan jaringan atau saat decoding JSON
+      return {
+        'succes': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
+  }
 }
